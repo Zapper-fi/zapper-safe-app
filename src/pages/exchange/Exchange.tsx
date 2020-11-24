dc56cb8faec41d16f29acb7538c6b725b87959c3
Karan Shahani <karan.shahani91@gmail.com>
2020-11-24 18:01:27 -0500

    Convert exchange to SCSS again

--- src/pages/exchange/Exchange.tsx
+++ src/pages/exchange/Exchange.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Card, Text, Button, Divider } from '@gnosis.pm/safe-react-components';
-import { Box, Grid } from '@material-ui/core';
 import { MdSwapVert } from 'react-icons/md';
 import styled from 'styled-components';
 
@@ -11,36 +10,6 @@ import { ExchangeAction, ModalType } from './ExchangeProvider';
 import { useExchangeDispatch } from './hooks/useExchangeDispatch';
 import { useExchangeState } from './hooks/useExchangeState';
 import { SelectTokenModal } from './SelectTokenModal';
-
-const ExchangeCard = styled(Card)`
-  max-width: 500px;
-  margin: 0 auto;
-  padding: 16px;
-`;
-
-const SwapButton = styled.button`
-  background: #008c73;
-  font-family: 'Avenir Next';
-  font-size: 16px;
-  font-weight: 500;
-  border: none;
-  border-radius: 100%;
-  outline: none;
-  transition: 0.3s;
-  padding: 8px;
-  cursor: pointer;
-  white-space: nowrap;
-  color: #f7f5f5;
-  box-shadow: #f7f5f5;
-
-  &:hover {
-    box-shadow: yellow;
-  }
-
-  .icon {
-    color: #f7f5f5;
-  }
-`;
 
 const TokenSelectButton = styled(Button)`
   display: flex;
@@ -59,29 +28,23 @@ const TokenSelectButton = styled(Button)`
   }
 `;
 
-const ExchangeButton = styled(Button)`
-  width: 100%;
-`;
-
 export const Exchange: React.FC = () => {
   const { tokenToSell, tokenToBuy } = useExchangeState();
   const dispatch = useExchangeDispatch();
 
   return (
     <Page>
-      <ExchangeCard>
-        <Box py={1}>
-          <Grid container direction="row" justify="space-between" alignItems="center">
+      <div className="exchange">
+        <Card className="exchange_card">
+          <div className="py-2 flex flex-baseline flex-between">
             <Text size="xl" color="secondaryHover">
               From
             </Text>
             <Text size="lg" color="secondaryHover">
               Balance: 0
             </Text>
-          </Grid>
-        </Box>
-        <Box py={1}>
-          <Grid container direction="row" justify="space-between" alignItems="center">
+          </div>
+          <div className="py-2 flex flex-baseline flex-between">
             <TokenSelectButton
               size="md"
               color="primary"
@@ -90,43 +53,45 @@ export const Exchange: React.FC = () => {
               <img src={`https://zapper.fi/images/${tokenToSell?.symbol}-icon.png`} />
               <span>{tokenToSell?.symbol}</span>
             </TokenSelectButton>
-          </Grid>
-        </Box>
-        <Divider />
-        <Box py={1}>
-          <SwapButton>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <MdSwapVert size="1.5em" />
+          </div>
+          <Divider />
+          <div className="py-2">
+            <button className="exchange_swap">
+              <div className="icon">
+                <div className="flex flex-center">
+                  <MdSwapVert size="1.5em" />
+                </div>
+              </div>
+            </button>
+          </div>
+          <Divider />
+          <div className="py-2">
+            <div className="py-2 flex flex-baseline flex-between">
+              <Text size="xl" color="secondaryHover">
+                To
+              </Text>
             </div>
-          </SwapButton>
-        </Box>
-        <Divider />
-        <Box py={1}>
-          <Grid container direction="row" justify="space-between" alignItems="center">
-            <Text size="xl" color="secondaryHover">
-              To
-            </Text>
-          </Grid>
-        </Box>
-        <Box py={1}>
-          <Grid container direction="row" justify="space-between" alignItems="center">
-            <TokenSelectButton
-              size="md"
-              color="primary"
-              onClick={() => dispatch({ type: ExchangeAction.SET_OPENED_MODAL, payload: ModalType.TO })}
-            >
-              {tokenToBuy && <img src={`https://zapper.fi/images/${tokenToSell?.symbol}-icon.png`} />}
-              <span>{tokenToBuy ? tokenToBuy.symbol : 'Select Token'}</span>
-            </TokenSelectButton>
-          </Grid>
-        </Box>
-        <Divider />
-        <Box py={1}>
-          <ExchangeButton size="lg" color="primary" variant="contained">
-            Exchange
-          </ExchangeButton>
-        </Box>
-      </ExchangeCard>
+          </div>
+          <div className="py-2">
+            <div className="py-2 flex flex-baseline flex-between">
+              <TokenSelectButton
+                size="md"
+                color="primary"
+                onClick={() => dispatch({ type: ExchangeAction.SET_OPENED_MODAL, payload: ModalType.TO })}
+              >
+                {tokenToBuy && <img src={`https://zapper.fi/images/${tokenToSell?.symbol}-icon.png`} />}
+                <span>{tokenToBuy ? tokenToBuy.symbol : 'Select Token'}</span>
+              </TokenSelectButton>
+            </div>
+          </div>
+          <Divider />
+          <div className="py-2">
+            <Button className="input--max" size="lg" color="primary" variant="contained">
+              Exchange
+            </Button>
+          </div>
+        </Card>
+      </div>
       <SelectTokenModal />
     </Page>
   );
